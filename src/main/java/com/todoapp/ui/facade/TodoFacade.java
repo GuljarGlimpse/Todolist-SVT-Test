f24abe07b315b4523b589fef4229ab77ba4c48d2
murad <138372292+itsmuradd@users.noreply.github.com>
2025-12-11 10:37:07 +0600

    Update TodoFacade.java

--- src/main/java/com/todoapp/ui/facade/TodoFacade.java
+++ src/main/java/com/todoapp/ui/facade/TodoFacade.java
@@ -1,1 +1,25 @@
+package com.todoapp.ui.facade;
 
+import com.todoapp.service.TodoService;
+import com.todoapp.model.Todo;
+import java.util.List;
+
+public class TodoFacade {
+    private TodoService todoService;
+
+    public TodoFacade() {
+        this.todoService = TodoService.getInstance();
+    }
+
+    public List<Todo> getAll() {
+        return todoService.getAllTodos();
+    }
+
+    public List<Todo> getPending() {
+        return todoService.getPendingTodos();
+    }
+
+    public List<Todo> getCompleted() {
+        return todoService.getCompletedTodos();
+    }
+}

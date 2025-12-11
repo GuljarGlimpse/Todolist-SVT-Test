68b8490ae3acf54fd3b3845f302ea88f0633f97b
Guljar Hosen <68489270+GuljarGlimpse@users.noreply.github.com>
2025-12-11 10:52:21 +0600

    Add ToggleStatusCommand to manage Todo status
    Implemented the ToggleStatusCommand to toggle the completion status of a Todo item based on its current status.

--- src/main/java/com/todoapp/service/command/ToggleStatusCommand.java
+++ src/main/java/com/todoapp/service/command/ToggleStatusCommand.java
@@ -1,1 +1,27 @@
+package com.todoapp.service.command;
 
+import com.todoapp.service.TodoService;
+
+public class ToggleStatusCommand implements Command {
+    private TodoService service;
+    private int todoId;
+
+    public ToggleStatusCommand(TodoService service, int todoId) {
+        this.service = service;
+        this.todoId = todoId;
+    }
+
+    @Override
+    public void execute() {
+        // We check the current status and flip it
+        // Note: This logic assumes your Service has findTodoById
+        var todo = service.findTodoById(todoId);
+        if (todo.isPresent()) {
+            if ("COMPLETED".equals(todo.get().getStatus())) {
+                service.uncompleteTodo(todoId);
+            } else {
+                service.completeTodo(todoId);
+            }
+        }
+    }
+}

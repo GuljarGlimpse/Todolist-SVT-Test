ae86114e7cdcd1a7cf523923f5db9aa61e60e3cd
murad <138372292+itsmuradd@users.noreply.github.com>
2025-12-11 10:37:52 +0600

    Update TodoInvoker.java

--- src/main/java/com/todoapp/ui/command/TodoInvoker.java
+++ src/main/java/com/todoapp/ui/command/TodoInvoker.java
@@ -1,1 +1,14 @@
+package com.todoapp.ui.command;
 
+import com.todoapp.service.command.Command;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoInvoker {
+    private List<Command> commandHistory = new ArrayList<>();
+
+    public void execute(Command command) {
+        commandHistory.add(command);
+        command.execute();
+    }
+}

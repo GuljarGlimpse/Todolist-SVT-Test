68710e5e4fda40dd5bf878bf29dbbe05b3a61a7f
Guljar Hosen <68489270+GuljarGlimpse@users.noreply.github.com>
2025-12-11 10:18:52 +0600

    Implement AddTodoCommand to add new todos

--- src/main/java/com/todoapp/service/command/AddTodoCommand.java
+++ src/main/java/com/todoapp/service/command/AddTodoCommand.java
@@ -1,1 +1,20 @@
+package com.todoapp.service.command;
 
+import com.todoapp.service.TodoService;
+
+public class AddTodoCommand implements Command {
+    private TodoService service;
+    private String title;
+    private String description;
+
+    public AddTodoCommand(TodoService service, String title, String description) {
+        this.service = service;
+        this.title = title;
+        this.description = description;
+    }
+
+    @Override
+    public void execute() {
+        service.addTodo(title, description);
+    }
+}

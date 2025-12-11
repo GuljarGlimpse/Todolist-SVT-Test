bedd21e42d1d25c06f303be0f075868d96b9f678
Guljar Hosen <68489270+GuljarGlimpse@users.noreply.github.com>
2025-12-11 10:19:45 +0600

    Implement DeleteTodoCommand class

--- src/main/java/com/todoapp/service/command/DeleteTodoCommand.java
+++ src/main/java/com/todoapp/service/command/DeleteTodoCommand.java
@@ -1,1 +1,18 @@
+package com.todoapp.service.command;
 
+import com.todoapp.service.TodoService;
+
+public class DeleteTodoCommand implements Command {
+    private TodoService service;
+    private int id;
+
+    public DeleteTodoCommand(TodoService service, int id) {
+        this.service = service;
+        this.id = id;
+    }
+
+    @Override
+    public void execute() {
+        service.deleteTodo(id);
+    }
+}
